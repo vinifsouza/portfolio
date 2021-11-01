5520b6c5ae517bb0454dc3c3c40af00cd1a0ea4b
Vinicius Souza <vinicius.fernando.souza@gmail.com>
2021-11-01 03:05:29 -0300

    notifica estado do build no Discord

--- src/components/Skills/SpaceAnimation/index.tsx
+++ src/components/Skills/SpaceAnimation/index.tsx
@@ -3,19 +3,15 @@ import React from 'react';
 import animationData from './space.json';
 import styled from 'styled-components';
 
-const SpaceWrapper = styled.span``;
-
 export default function SpaceAnimation() {
   return (
-    <SpaceWrapper>
-      <Lottie
-        style={{ width: 512, height: 512 }}
-        loop
-        play
-        animationData={animationData}
-        speed={0.1}
-        segments={[150, 240]}
-      />
-    </SpaceWrapper>
+    <Lottie
+      style={{ width: 512, height: 512 }}
+      loop
+      play
+      animationData={animationData}
+      speed={0.3}
+      segments={[150, 240]}
+    />
   );
 }
