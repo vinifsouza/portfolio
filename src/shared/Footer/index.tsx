440efc8ae26e9adaf3447bc80e7543002c5923fb
Vinícius Souza <vinicius.fernando.souza@gmail.com>
2021-11-22 16:53:54 -0300

    fix(chat): corrige implementação do chatbot

--- src/shared/Footer/index.tsx
+++ src/shared/Footer/index.tsx
@@ -3,19 +3,20 @@ import React, { useEffect } from 'react';
 import { DIRECTION_OPTIONS } from '../../constants/scrollIcon';
 import { ScrollToIcon } from './../ScrollToIcon/index';
 
-const blip = require("blip-chat-widget");
-
+const { BlipChat } = require('blip-chat-widget');
 
 export function Footer(prop: { target: string; direction: DIRECTION_OPTIONS }) {
   useEffect(() => {
-    new blip.BlipChat()
-        .withAppKey(
-          'cG9ydGZvbGlvMjo2ZTg2ZjU1Yy0wN2FkLTRkMTItYTQ3Zi04N2ZmMThhNjEzYzQ=',
-        )
-        .withButton({ color: '#4e148c', icon: '' })
-        .withCustomCommonUrl('https://vinicius-souza-vdzec.chat.blip.ai/')
-        .build();
-  }, [])
+    (function () {
+      window.onload = function () {
+          new BlipChat()
+          .withAppKey('cG9ydGZvbGlvMjo2ZTg2ZjU1Yy0wN2FkLTRkMTItYTQ3Zi04N2ZmMThhNjEzYzQ=')
+          .withButton({"color":"#4e148c"})
+          .withCustomCommonUrl('https://vinicius-souza-vdzec.chat.blip.ai/')
+          .build();
+      }
+  })();
+  }, []);
 
   return (
     <>
