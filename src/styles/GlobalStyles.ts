20bf4fd6ff694fe9979d66827a64846efd9c0d80
Vinícius Souza <vinicius.fernando.souza@gmail.com>
2022-07-24 16:22:57 -0300

    refactor(App): change global font-size 1rem to 1px

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,10 +1,13 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
+  html {
+    font-size: 62.5%; // 1rem = 1px;
+  }
+
   body, html {
     height: 100%;
     width: 100%;
-    font-size: 16px;
   }
 
   * {
@@ -16,75 +19,33 @@ export const GlobalStyles = createGlobalStyle`
   }
 
 
-  // Device = Desktops
-  // Screen = 1281px to higher resolution desktops
-  @media (min-width: 1280px) {
-    h1 {
-      font-size: 4rem;
-    }
+  h1 {
+    font-size: 4rem;
+  }
 
-    h2 {
-      font-size: 2rem;
-    }
+  h2 {
+    font-size: 2rem;
+  }
 
+  p, nav {
+    font-size: 2rem;
+  }
+
+  @media (max-width: 768px) {
     p, nav {
-      font-size: 1rem;
+      font-size: 1.6rem;
     }
   }
 
-  // Device = Laptops, Desktops
-  // Screen = B/w 1025px to 1280px */
-  @media (min-width: 1025px) and (max-width: 1279px) {
-    h1 {
-      font-size: 3rem;
-    }
-
-    h2 {
-      font-size: 1.5rem;
-    }
-
+  @media (max-width: 425px) {
     p, nav {
-      font-size: 0.9rem;
+      font-size: 1.4rem;
     }
   }
 
-  // Device = Tablets, Ipads (portrait)
-  // Screen = B/w 768px to 1024px
-  @media (min-width: 768px) and (max-width: 1024px) {
-    h1 {
-      font-size: 3rem;
-    }
-
-    h2 {
-      font-size: 2rem;
-    }
-
+  @media (max-width: 320px) {
     p, nav {
-      font-size: 0.9rem;
+      font-size: 1.2rem;
     }
-  }
-
-  // Device = Tablets, Ipads (landscape)
-  // Screen = B/w 768px to 1024px
-  @media (min-width: 768px) and (max-width: 1024px) and (orientation: landscape) {
-
-    /* CSS */
-
-  }
-
-  // Device = Low Resolution Tablets, Mobiles (Landscape)
-  // Screen = B/w 481px to 767px
-  @media (min-width: 481px) and (max-width: 767px) {
-
-    /* CSS */
-
-  }
-
-  // Device = Most of the Smartphones Mobiles (Portrait)
-  // Screen = B/w 320px to 479px
-  @media (min-width: 320px) and (max-width: 480px) {
-
-    /* CSS */
-
   }
 `;
